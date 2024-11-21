428afee61cc7d25f02425e1ba9eb04500c439585
s218270 <mibant@onet.pl>
2024-11-21 17:09:14 +0100

    Added font settings

--- src/app/components/reader/reader.component.ts
+++ src/app/components/reader/reader.component.ts
@@ -1,6 +1,6 @@
 //reader component ts
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { MatSelectModule } from '@angular/material/select';
@@ -31,10 +31,62 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './reader.component.html',
   styleUrl: './reader.component.scss',
 })
-export class ReaderComponent {
+export class ReaderComponent implements OnInit{
   sidenavOpened = false;
+  openFontSettings = false;
   readingMode: 'bionic' | 'static' = 'bionic';
   fontSize = 16;
   bgColor = '#ffffff';
+  selectedFontFamily = 'Roboto'; // Default font
+  fonts: string[] = [
+    'Roboto', 
+    'Great Vibes', 
+    'Cinzel Decorative', 
+    'Shadows Into Light', 
+    'Pacifico', 
+    'Playfair Display', 
+    'Lobster', 
+    'Courier Prime', 
+    'Comic Neue'
+  ];
+  selectedFontSize = 16;
+  fontSizes: number[] = [
+    8, 9, 10,
+    11, 12, 13,
+    14, 15, 16,
+    17, 18, 19,
+    20, 21, 22,
+    23, 24, 25
+  ]
   fontColor = '#000000';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+
+    ngOnInit(): void {
+         // Set default font on component initialization
+    this.applyFontFamily(this.selectedFontFamily);
+    }
+
+    onFontChange(): void {
+        // Apply the selected font to the content dynamically
+        this.applyFontFamily(this.selectedFontFamily);
+      }
+    
+      applyFontFamily(font: string): void {
+        if (isPlatformBrowser(this.platformId)) {
+          document.documentElement.style.setProperty('--selected-font', font);
+        }
+      }
+
+      onFontSizeChange(): void {
+        // Apply the selected font to the content dynamically
+        this.applyFontSize(this.selectedFontSize);
+      }
+
+      applyFontSize(size: number): void {
+        if (isPlatformBrowser(this.platformId)) {
+          document.documentElement.style.setProperty('--selected-size', `${size}px`);
+        }
+      }
 }
